8fbe4952cc060dfd1deb7978799e81c82f14b9d3
Václav Pelíšek <peldax@gmail.com>
2017-12-02 01:30:51 +0100

    updated breadcrumbs

--- src/Component/Breadcrumb.php
+++ src/Component/Breadcrumb.php
@@ -4,4 +4,36 @@ namespace Peldax\NetteInit\Component;
 
 class Breadcrumb extends BaseComponent
 {
+    public function render()
+    {
+        $module = $this->getPresenter()->getModule();
+        $presenter = $this->getPresenter()->getNameWM();
+        $action = $this->getPresenter()->getAction();
+
+        $breadcrumbs = [];
+
+        if (class_exists('\App\AppModule\Presenter\DefaultPresenter'))
+        {
+            $breadcrumbs[':App:Default:default'] = 'Home';
+        }
+
+        if ($module !== 'App')
+        {
+            $breadcrumbs['Default:default'] = $module;
+        }
+
+        if ($presenter !== 'Default')
+        {
+            $breadcrumbs[':default'] = $presenter;
+        }
+
+        if ($action !== 'default')
+        {
+            $breadcrumbs['X'] = ucfirst($action);
+        }
+
+        $this->template->breadcrumbs = $breadcrumbs;
+
+        parent::render();
+    }
 }
