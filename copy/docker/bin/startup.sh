c05dfa68c96e9d4c0f6d04684032c8c0cf07586d
Václav Pelíšek <peldax@gmail.com>
2017-12-27 16:27:24 +0100

    format messages

--- copy/docker/bin/startup.sh
+++ copy/docker/bin/startup.sh
@@ -4,38 +4,38 @@ cd /var/www/html
 
 if [ ! -h node_modules ]
 then
-    printf '\nCreating node_modules symlink\n'̈́
+    printf '\n---> Creating node_modules symlink\n\n'̈́
     ln -s www/node_modules node_modules
 fi
 
-printf '\nRunning Composer\n'
+printf '\n---> Running Composer\n\n'
 export COMPOSER_ALLOW_SUPERUSER=1
 composer update --no-interaction
 
-printf '\nRunning NPM\n'
+printf '\n---> Running NPM\n\n'
 npm update
 chmod +x /var/www/html/node_modules/uglify-es/bin/uglifyjs
 
-printf '\nCompiling SCSS\n'
+printf '\n---> Compiling SCSS\n\n'
 sh /usr/local/bin/scss.sh
 
-printf '\nMinifying JS\n'
+printf '\n---> Minifying JS\n\n'
 sh /usr/local/bin/js.sh
 
 if [ ! -f /etc/ssl/private/server.key ] || [ ! -f /etc/ssl/certs/server.crt ]
 then
-    printf '\nGenerating SSL certificate\n'
+    printf '\n---> Generating SSL certificate\n\n'
     openssl req \
         -newkey rsa:4096 -nodes -sha256 -keyout /etc/ssl/private/server.key \
         -x509 -days 365 -out /etc/ssl/certs/server.crt \
         -subj "/C=CZ/ST=Czech Republic/L=Peldax/O=Peldax/OU=Peldax/CN=localhost.com"
 fi
 
-printf '\nFixing permissions\n'
+printf '\n---> Fixing permissions\n\n'
 chown -R :www-data /var/log/apache2
 chown -R :www-data /var/www/html
 find /var/www/html \( -type f -execdir chmod 660 {} \; \) \
                 -o \( -type d -execdir chmod 770 {} \; \)
 
-printf '\nStarting Apache\n'
+printf '\n---> Starting Apache\n\n'
 apache2ctl -D FOREGROUND
