9ce69104455b4ac91e00c7eeba103fe413e3a526
Václav Pelíšek <peldax@gmail.com>
2017-12-27 16:15:23 +0100

    docker changes

--- copy/docker/bin/js.sh
+++ copy/docker/bin/js.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for FILE in `find /var/www/html/www -type f -name '*.js' -not -name '*.min.js'`
+for FILE in `find /var/www/html/www/js -type f -name '*.js' -not -name '*.min.js'`
 do
     /var/www/html/node_modules/.bin/uglifyjs "${FILE}" > "${FILE%.js}.min.js"
 done
--- copy/docker/bin/startup.sh
+++ copy/docker/bin/startup.sh
@@ -2,38 +2,40 @@
 
 cd /var/www/html
 
-if [ ! -e node_modules ]
+if [ ! -h node_modules ]
 then
-    echo 'Create symlink'̈́
+    printf '\nCreating node_modules symlink\n'̈́
     ln -s www/node_modules node_modules
 fi
 
-echo 'Composer'
+printf '\nRunning Composer\n'
 export COMPOSER_ALLOW_SUPERUSER=1
-composer update --no-interaction &> /dev/null
+composer update --no-interaction
 
-echo 'NPM'
-npm update &> /dev/null
+printf '\nRunning NPM\n'
+npm update
+chmod +x /var/www/html/node_modules/uglify-es/bin/uglifyjs
 
-echo 'SCSS'
+printf '\nCompiling SCSS\n'
 sh /usr/local/bin/scss.sh
 
-echo 'JS'
+printf '\nMinifying JS\n'
 sh /usr/local/bin/js.sh
 
-if [ ! -e /etc/ssl/private/server.key ] || [ ! -e /etc/ssl/certs/server.crt ]
+if [ ! -f /etc/ssl/private/server.key ] || [ ! -f /etc/ssl/certs/server.crt ]
 then
-    echo 'Generate SSL certificate'
+    printf '\nGenerating SSL certificate\n'
     openssl req \
         -newkey rsa:4096 -nodes -sha256 -keyout /etc/ssl/private/server.key \
         -x509 -days 365 -out /etc/ssl/certs/server.crt \
         -subj "/C=CZ/ST=Czech Republic/L=Peldax/O=Peldax/OU=Peldax/CN=localhost.com"
 fi
 
+printf '\nFixing permissions\n'
 chown -R :www-data /var/log/apache2
 chown -R :www-data /var/www/html
 find /var/www/html \( -type f -execdir chmod 660 {} \; \) \
                 -o \( -type d -execdir chmod 770 {} \; \)
 
-echo 'Apache start'
+printf '\nStarting Apache\n'
 apache2ctl -D FOREGROUND
