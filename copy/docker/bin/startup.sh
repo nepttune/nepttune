3a1439ebd03dd34c1d9429027623d6b5f4d2dadc
Václav Pelíšek <peldax@gmail.com>
2017-12-27 15:40:37 +0100

    docker startup

--- copy/docker/bin/startup.sh
+++ copy/docker/bin/startup.sh
@@ -2,17 +2,38 @@
 
 cd /var/www/html
 
-if [ ! -e node_modules ]; then
+if [ ! -e node_modules ]
+then
+    echo 'Create symlink'̈́
     ln -s www/node_modules node_modules
 fi
 
-composer update --no-interaction
-npm update
+echo 'Composer'
+export COMPOSER_ALLOW_SUPERUSER=1
+composer update --no-interaction &> /dev/null
 
+echo 'NPM'
+npm update &> /dev/null
+
+echo 'SCSS'
 sh /usr/local/bin/scss.sh
+
+echo 'JS'
 sh /usr/local/bin/js.sh
 
-chown -R www-data /var/log/apache2
-chown -R www-data /var/www/html
+if [ ! -e /etc/ssl/private/server.key ] || [ ! -e /etc/ssl/certs/server.crt ]
+then
+    echo 'Generate SSL certificate'
+    openssl req \
+        -newkey rsa:4096 -nodes -sha256 -keyout /etc/ssl/private/server.key \
+        -x509 -days 365 -out /etc/ssl/certs/server.crt \
+        -subj "/C=CZ/ST=Czech Republic/L=Peldax/O=Peldax/OU=Peldax/CN=localhost.com"
+fi
 
+chown -R :www-data /var/log/apache2
+chown -R :www-data /var/www/html
+find /var/www/html \( -type f -execdir chmod 660 {} \; \) \
+                -o \( -type d -execdir chmod 770 {} \; \)
+
+echo 'Apache start'
 apache2ctl -D FOREGROUND
