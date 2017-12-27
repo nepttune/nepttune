efd2c37f1f155df452ded57b7232c9ff7f014543
Václav Pelíšek <peldax@gmail.com>
2017-12-27 17:01:25 +0100

    detect changes of docker files and throw warning

--- src/Deploy.php
+++ src/Deploy.php
@@ -4,8 +4,16 @@ namespace Peldax\NetteInit;
 
 final class Deploy
 {
-    const PROJECT_DIR = __DIR__ . '/../../../../';
-    const FILES_DIR = __DIR__ . '/../copy/';
+    const TARGET_DIR = __DIR__ . '/../../../../';
+    const SOURCE_DIR = __DIR__ . '/../copy/';
+
+    const DOCKER_TARGET_DIR = self::TARGET_DIR . 'docker/';
+    const DOCKER_SOURCE_DIR = self::SOURCE_DIR . 'docker/';
+    const DOCKER_FILES = [
+        'bin/startup.sh',
+        'docker-compose.yml',
+        'dockerfile-apache-php'
+    ];
 
     const DIRS = [
         'app/model',
@@ -22,7 +30,7 @@ final class Deploy
     {
         echo 'Peldax\Init handler started.' . PHP_EOL;
 
-        self::recurseCopy(self::FILES_DIR, self::PROJECT_DIR);
+        self::recurseCopy(self::SOURCE_DIR, self::TARGET_DIR);
 
         self::createDirs();
 
@@ -33,21 +41,41 @@ final class Deploy
     {
         $dir = opendir($src);
 
+        if ($src === self::DOCKER_SOURCE_DIR)
+        {
+            $change = false;
+
+            foreach (self::DOCKER_FILES as $file)
+            {
+                if (!file_exists(self::DOCKER_TARGET_DIR . $file) ||
+                    hash_file('sha256̈́', self::DOCKER_SOURCE_DIR . $file) === hash_file('sha256', self::DOCKER_TARGET_DIR . $file))
+                {
+                    $change = true;
+                    break;
+                }
+            }
+
+            if ($change)
+            {
+                echo "\033[31m WARNING: Docker files has changed, reload docker container. \033[0m" . PHP_EOL;
+            }
+        }
+
         if (!is_dir($dst))
         {
             mkdir($dst);
         }
 
-        while(false !== ( $file = readdir($dir)) )
+        while(false !== ($file = readdir($dir)))
         {
-            if (( $file !== '.' ) && ( $file !== '..' ))
+            if (($file !== '.') && ($file !== '..'))
             {
-                if ( is_dir($src . '/' . $file) )
+                if (is_dir($src . '/' . $file))
                 {
                     self::recurseCopy($src . '/' . $file,$dst . '/' . $file);
                 }
                 else
-                    {
+                {
                     copy($src . '/' . $file,$dst . '/' . $file);
                 }
             }
