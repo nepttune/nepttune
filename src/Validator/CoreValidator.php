a7dea7b62739d4e55ec281acae5746812b73529d
Václav Pelíšek <peldax@gmail.com>
2017-12-31 02:33:23 +0100

    Update CoreValidator.php

--- src/Validator/CoreValidator.php
+++ src/Validator/CoreValidator.php
@@ -4,13 +4,7 @@ namespace Peldax\NetteInit\Validator;
 
 final class CoreValidator
 {
-    const LENGTH = 'Peldax\NetteInit\Validator\CoreValidator::length';
     const SAME_LENGTH = 'Peldax\NetteInit\Validator\CoreValidator::sameLength';
-    
-    public static function length(\Nette\Forms\IControl $control, string $controlName)
-    {
-        return strlen($control->getValue()) == $control->getForm()[$controlName]->getValue();
-    }
     
     public static function sameLength(\Nette\Forms\IControl $control, string $controlName)
     {
