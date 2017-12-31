42f94dae766251ece5da06c34d8d99003fc5ab4f
Václav Pelíšek <peldax@gmail.com>
2017-12-31 02:17:10 +0100

    removed obsolete validators

--- src/Validator/CoreValidator.php
+++ src/Validator/CoreValidator.php
@@ -4,32 +4,8 @@ namespace Peldax\NetteInit\Validator;
 
 final class CoreValidator
 {
-    const GREATER_EQUAL = 'Peldax\NetteInit\Validator\CoreValidator::greaterEqual';
-    const GREATER = 'Peldax\NetteInit\Validator\CoreValidator::greater';
-    const LESS_EQUAL = 'Peldax\NetteInit\Validator\CoreValidator::lessEqual';
-    const LESS = 'Peldax\NetteInit\Validator\CoreValidator::less';
     const LENGTH = 'Peldax\NetteInit\Validator\CoreValidator::length';
     const SAME_LENGTH = 'Peldax\NetteInit\Validator\CoreValidator::sameLength';
-
-    public static function greaterEqual(\Nette\Forms\IControl $control, string $controlName)
-    {
-        return $control->getValue() >= $control->getForm()[$controlName]->getValue();
-    }
-
-    public static function greater(\Nette\Forms\IControl $control, string $controlName)
-    {
-        return $control->getValue() > $control->getForm()[$controlName]->getValue();
-    }
-
-    public static function lessEqual(\Nette\Forms\IControl $control, string $controlName)
-    {
-        return $control->getValue() <= $control->getForm()[$controlName]->getValue();
-    }
-
-    public static function less(\Nette\Forms\IControl $control, string $controlName)
-    {
-        return $control->getValue() < $control->getForm()[$controlName]->getValue();
-    }
     
     public static function length(\Nette\Forms\IControl $control, string $controlName)
     {
