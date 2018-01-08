e73eae0abebba9678dc0df23ebc2c478b9327617
Václav Pelíšek <peldax@gmail.com>
2018-01-08 15:48:08 +0100

    Update RouterFactory.php

--- src/RouterFactory.php
+++ src/RouterFactory.php
@@ -10,11 +10,27 @@ final class RouterFactory
     /**
      * @return \Nette\Application\IRouter
      */
-    public static function createRouter()
+    public static function createSubdomainRouter()
     {
         $router = new RouteList();
 
         $router[] = new Route('//<module>.%domain%/[<lang [a-z]{2}>/]<presenter>/<action>[/<id>]', [
+            'presenter' => 'Default',
+            'action' => 'default'
+        ]);
+
+        return $router;
+    }
+
+    /**
+     * @return \Nette\Application\IRouter
+     */
+    public static function createStandardRouter()
+    {
+        $router = new RouteList();
+
+        $router[] = new Route('/[<lang [a-z]{2}>/]<module>/<presenter>/<action>[/<id>]', [
+            'module' => 'Www',
             'presenter' => 'Default',
             'action' => 'default'
         ]);
