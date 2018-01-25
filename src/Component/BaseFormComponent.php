fdfffa403c3e2c52e57798748e7a57fb29ac4da8
Václav Pelíšek <peldax@gmail.com>
2018-01-25 11:54:00 +0100

    Update BaseFormComponent.php

--- src/Component/BaseFormComponent.php
+++ src/Component/BaseFormComponent.php
@@ -6,7 +6,15 @@ use \Nette\Application\UI\Form;
 
 abstract class BaseFormComponent extends BaseComponent
 {
+    const SAVE_NEXT = 1;
+    const SAVE_LIST = 1;
+
     const REDIRECT = ':default';
+    const REDIRECT_ID = 0;
+    const REDIRECT_NEXT = ':add';
+    const REDIRECT_NEXT_ID = 0;
+    const REDIRECT_LIST = ':list';
+    const REDIRECT_LIST_ID = 0;
     
     const PATTERN_PHONE = '^[+(]{0,2}[0-9 ().-]{9,}';
 
@@ -36,6 +44,16 @@ abstract class BaseFormComponent extends BaseComponent
         $form->addSubmit('submit', 'global.save');
         $form->onSuccess[] = [$this, 'formSuccess'];
 
+        if (static::SAVE_NEXT)
+        {
+            $form->addSubmit('save_next', 'global.save_next');
+        }
+
+        if (static::SAVE_LIST)
+        {
+            $form->addSubmit('save_list', 'global.save_list');
+        }
+        
         return $form;
     }
 
@@ -43,10 +61,31 @@ abstract class BaseFormComponent extends BaseComponent
 
     public function formSuccess(Form $form, \stdClass $values) : void
     {
-        $this->repository->save($values);
+        if ($this->getPresenter()->getAction() === 'edit')
+        {
+            $values->id = $this->getPresenter()->getId();
+        }
+        
+        $row = $this->repository->save($values);
+
+        $params = [];
+        switch ($form->isSubmitted()->name)
+        {
+            case 'save_next':
+                if (static::REDIRECT_NEXT_ID) { $params = ['id' => $row->id]; }
+                $redirect = static::REDIRECT_NEXT;
+                break;
+            case 'save_list':
+                if (static::REDIRECT_LIST_ID) { $params = ['id' => $row->id]; }
+                $redirect = static::REDIRECT_LIST;
+                break;
+            default:
+                if (static::REDIRECT_ID) { $params = ['id' => $row->id]; }
+                $redirect = static::REDIRECT;
+        }
 
         $this->getPresenter()->flashMessage($this->translator->translate('global.flash.save_success'), 'success');
-        $this->getPresenter()->redirect(static::REDIRECT);
+        $this->getPresenter()->redirect($redirect, $params);
     }
 
     public function injectTranslator(\Kdyby\Translation\Translator $translator)
