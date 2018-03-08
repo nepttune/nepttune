e2335a6d86de089b6dc131e9d3bc6eb7dac3674a
Václav Pelíšek <peldax@gmail.com>
2018-03-09 00:24:32 +0100

    Update tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS `log_error`
   `id`         INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `ip_address` VARBINARY(16)               NOT NULL,
   `url`        VARCHAR(255)                NOT NULL,
+  `return_code`VARCHAR(10)                 NOT NULL,
   `datetime`   DATETIME                    NOT NULL
 );
