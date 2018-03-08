0250f5836bbf13cca20a1fd21484a32c1d083d98
Václav Pelíšek <peldax@gmail.com>
2018-03-09 00:34:23 +0100

    Update ErrorPresenter.php

--- src/Presenter/ErrorPresenter.php
+++ src/Presenter/ErrorPresenter.php
@@ -7,8 +7,26 @@ final class ErrorPresenter extends \Nepttune\Presenter\BasePresenter
     /** @var  \Nepttune\Model\ErrorLogModel */
     protected $errorLogModel;
 
+    /** @var \Nette\Http\Request */
+    protected $request;
+
+    public function __construct(
+        \Nepttune\Model\ErrorLogModel $errorLogModel,
+        \Nette\Http\Request $request)
+    {
+        $this->errorLogModel = $errorLogModel;
+        $this->request = $request;
+    }
+
     public function actionDefault($exception, $request)
     {
+        $this->errorLogModel->insert([
+            'datetime' => new \Nette\Utils\DateTime(),
+            'return_code' => $exception->getCode(),
+            'ip_address' => inet_pton($this->request->getRemoteAddress()),
+            'url' => $this->request->getUrl()
+        ]);
+        
         $this->template->code = $exception->getCode();
 
         if ($exception->getCode() >= 500)
