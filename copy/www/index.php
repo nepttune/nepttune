eda1b0c44827989d7e435cd68ba7cccf571dc502
Václav Pelíšek <peldax@gmail.com>
2018-03-18 15:03:59 +0100

    Update index.php

--- copy/www/index.php
+++ copy/www/index.php
@@ -2,5 +2,19 @@
 
 declare(strict_types = 1);
 
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']))
+{
+    if ($_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' && isset($_SERVER['SERVER_PORT']) && in_array($_SERVER['SERVER_PORT'], [80, 82]))
+    { // https over proxy
+        $_SERVER['HTTPS'] = 'On';
+        $_SERVER['SERVER_PORT'] = 443;
+    }
+    elseif ($_SERVER['HTTP_X_FORWARDED_PROTO'] === 'http' && isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 80)
+    { // http over proxy
+        $_SERVER['HTTPS'] = 'Off';
+        $_SERVER['SERVER_PORT'] = 80;
+    }
+}
+
 $container = require __DIR__ . '/../app/bootstrap.php';
 $container->getService('application')->run();
