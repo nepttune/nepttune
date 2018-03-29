f299aa969c87447525bfd1bf5dbd82ac156f2779
Václav Pelíšek <peldax@gmail.com>
2018-03-29 09:13:37 +0200

    Add files via upload

--- src/Component/AssetLoader.php
+++ src/Component/AssetLoader.php
@@ -2,7 +2,7 @@
 
 namespace Nepttune\Component;
 
-final class AssetLoader extends BaseComponent
+final class AssetLoader extends BaseComponent implements IStyleLists, IScriptLists
 {
     /** @var \Kdyby\Redis\RedisStorage */
     public $storage;
@@ -12,25 +12,188 @@ final class AssetLoader extends BaseComponent
         $this->storage = $redisStorage;
     }
 
-    public function renderScript()
+    /*** @var bool */
+    protected $admin;
+
+    /** @var  array */
+    protected $viewStyles = [];
+
+    /** @var array  */
+    protected $viewScripts = [];
+
+    public function attached($presenter)
     {
-        $this->beforeRender();
-        $this->template->setFile(__DIR__ . '/Script.latte');
-        $this->template->render();
+        $this->admin = $presenter instanceof \Nepttune\Presenter\BaseAuthPresenter;
+
+        $module = $presenter->getModule();
+        $presen = $presenter->getName();
+        $action = $presenter->getAction();
+
+        $moduleStyle = '/scss/module/' . $module . '.min.css';
+        if (file_exists(getcwd() . '/node_modules/nepttune' . $moduleStyle))
+        {
+            $this->viewStyles[] = '/node_modules/nepttune' . $moduleStyle;
+        }
+        if (file_exists(getcwd() . $moduleStyle))
+        {
+            $this->viewStyles[] = $moduleStyle;
+        }
+
+        $moduleScript = '/js/module/' . $module . '.min.js';
+        if (file_exists(getcwd() . '/node_modules/nepttune' . $moduleScript))
+        {
+            $this->viewScripts[] = '/node_modules/nepttune' . $moduleScript;
+        }
+        if (file_exists(getcwd() . $moduleScript))
+        {
+            $this->viewScripts[] = $moduleScript;
+        }
+
+        $presenStyle = '/scss/presenter/' . $presen . '.min.css';
+        if (file_exists(getcwd() . '/node_modules/nepttune' . $presenStyle))
+        {
+            $this->viewStyles[] = '/node_modules/nepttune' . $presenStyle;
+        }
+        if (file_exists(getcwd() . $presenStyle))
+        {
+            $this->viewStyles[] = $presenStyle;
+        }
+
+        $presenScript = '/js/presenter/' . $presen . '.min.js';
+        if (file_exists(getcwd() . '/node_modules/nepttune' . $presenScript))
+        {
+            $this->viewScripts[] = '/node_modules/nepttune' . $presenScript;
+        }
+        if (file_exists(getcwd() . $presenScript))
+        {
+            $this->viewScripts[] = $presenScript;
+        }
+
+        $actionStyle = '/scss/action/' . $presen . '/' . $action . '.min.css';
+        if (file_exists(getcwd() . '/node_modules/nepttune' . $actionStyle))
+        {
+            $this->viewStyles[] = '/node_modules/nepttune' . $actionStyle;
+        }
+        if (file_exists(getcwd() . $actionStyle))
+        {
+            $this->viewStyles[] = $actionStyle;
+        }
+
+        $actionScript = '/js/action/' . $presen . '/' . $action . '.min.js';
+        if (file_exists(getcwd() . '/node_modules/nepttune' . $actionScript))
+        {
+            $this->viewScripts[] = '/node_modules/nepttune' . $actionScript;
+        }
+        if (file_exists(getcwd() . $actionScript))
+        {
+            $this->viewScripts[] = $actionScript;
+        }
     }
 
-    public function renderStyle()
+    public function renderHead()
     {
-        $this->beforeRender();
-        $this->template->setFile(__DIR__ . '/Style.latte');
-        $this->template->render();
+        $styles = static::STYLE_HEAD;
+        $scripts = static::SCRIPT_HEAD;
+
+        if ($this->admin)
+        {
+            $styles = array_merge($styles, static::STYLE_HEAD_ADMIN);
+            $scripts = array_merge($scripts, static::SCRIPT_HEAD_ADMIN);
+        }
+        else
+        {
+            $styles = array_merge($styles, static::STYLE_HEAD_FRONT);
+            $scripts = array_merge($scripts, static::SCRIPT_HEAD_FRONT);
+        }
+
+        $styles = array_merge($styles, $this->viewStyles);
+
+        $this->template->variables = false;
+        $this->template->recaptcha = false;
+        $this->template->styles = $styles;
+        $this->template->scripts = $scripts;
+
+        parent::render();
     }
 
-    public function renderStyleComponent()
+    public function renderBody()
     {
-        $this->beforeRender();
-        $this->template->setFile(__DIR__ . '/Style_component.latte');
-        $this->template->render();
+        $styles = static::STYLE_BODY;
+        $scripts = static::SCRIPT_BODY;
+
+        if ($this->admin)
+        {
+            $styles = array_merge($styles, static::STYLE_BODY_ADMIN);
+            $scripts = array_merge($scripts, static::SCRIPT_BODY_ADMIN);
+        }
+        else
+        {
+            $styles = array_merge($styles, static::STYLE_BODY_FRONT);
+            $scripts = array_merge($scripts, static::SCRIPT_BODY_FRONT);
+        }
+
+        $scripts = array_merge($scripts, $this->viewScripts);
+
+        $hasForm = false;
+        $hasList = false;
+        $hasStat = false;
+
+        foreach ($this->getPresenter()->getComponents() as $name => $component)
+        {
+            $componentStyle = '/scss/component/' . $name . '.min.css';
+            $componentScript = '/js/component/' . $name . '.min.js';
+
+            if (file_exists(getcwd() . $componentStyle))
+            {
+                $styles[] = $componentStyle;
+            }
+
+            if (file_exists(getcwd() . $componentScript))
+            {
+                $scripts[] = $componentScript;
+            }
+
+            if (!$hasForm && strpos($name, 'Form') !== false)
+            {
+                $hasForm = true;
+            }
+
+            if (!$hasList && strpos($name, 'List') !== false)
+            {
+                $hasForm = true;
+                $hasList = true;
+            }
+
+            if (!$hasStat && strpos($name, 'Stat') !== false)
+            {
+                $hasStat = true;
+            }
+        }
+
+        if ($hasForm)
+        {
+            $styles = array_merge($styles, static::STYLE_FORM);
+            $scripts = array_merge($scripts, static::SCRIPT_FORM);
+        }
+
+        if ($hasList)
+        {
+            $styles = array_merge($styles, static::STYLE_LIST);
+            $scripts = array_merge($scripts, static::SCRIPT_LIST);
+        }
+
+        if ($hasStat)
+        {
+            $styles = array_merge($styles, static::STYLE_STAT);
+            $scripts = array_merge($scripts, static::SCRIPT_STAT);
+        }
+
+        $this->template->variables = true;
+        $this->template->recaptcha = $hasForm;
+        $this->template->styles = $styles;
+        $this->template->scripts = $scripts;
+
+        parent::render();
     }
 
     public function getIntegrity(string $path) : string
---
+++ src/Component/IScriptLists.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Nepttune\Component;
+
+interface IScriptLists
+{
+    const SCRIPT_HEAD = [];
+    const SCRIPT_HEAD_ADMIN = [];
+    const SCRIPT_HEAD_FRONT = [];
+
+    const SCRIPT_BODY = [
+        '/node_modules/jquery/dist/jquery.min.js',
+        '/node_modules/nette.ajax.js/nette.ajax.js',
+        '/node_modules/magnific-popup/dist/jquery.magnific-popup.min.js',
+        '/node_modules/nepttune/js/common.min.js'];
+    const SCRIPT_BODY_ADMIN = [
+        '/node_modules/bootstrap/dist/js/bootstrap.min.js',
+        '/node_modules/admin-lte/dist/js/adminlte.min.js'];
+    const SCRIPT_BODY_FRONT = [
+        '/node_modules/bootstrap-beta/dist/js/bootstrap.bundle.min.js'];
+
+    const SCRIPT_FORM = [
+        '/node_modules/nette-forms/src/assets/netteForms.min.js',
+        '/node_modules/pickadate/lib/picker.js',
+        '/node_modules/pickadate/lib/picker.date.js',
+        '/node_modules/pickadate/lib//translations/cs_CZ.js',
+        '/node_modules/select2/dist/js/select2.min.js',
+        '/node_modules/icheck/icheck.min.js',
+        '/node_modules/nas-ext-dependent-select-box/client-side/dependentSelectBox.js',
+        '/node_modules/nextras-forms/js/nextras.netteForms.js',
+        '/node_modules/nepttune/js/coreValidator.min.js',
+        '/node_modules/nepttune/js/form.min.js'];
+    const SCRIPT_LIST = [
+        '/node_modules/ublaboo-datagrid/assets/dist/datagrid.min.js',
+        '/node_modules/ublaboo-datagrid/assets/dist/datagrid-spinners.min.js'];
+    const SCRIPT_STAT = [
+        '/node_modules/chart.js/dist/Chart.min.js',
+        '/node_modules/nepttune/js/stat.min.js'];
+}
