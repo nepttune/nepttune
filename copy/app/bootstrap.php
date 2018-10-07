baea04c4fcd0ff373ab207c685c23eec0a4dcf2a
Václav Pelíšek <peldax@gmail.com>
2018-10-08 00:27:40 +0200

    Update bootstrap.php

--- copy/app/bootstrap.php
+++ copy/app/bootstrap.php
@@ -30,16 +30,22 @@ foreach (['nepttune', 'admin'] as $extension)
     $coreFile = __DIR__ . "/../vendor/nepttune/{$extension}/config/core.neon";
     $debugFile = __DIR__ . "/../vendor/nepttune/{$extension}/config/debug.neon";
 
-    if (file_exists($coreFile))
+    if (\file_exists($coreFile))
     {
         $configurator->addConfig($coreFile);
     }
 
-    if ($debugMode && file_exists($debugFile))
+    if ($debugMode && \file_exists($debugFile))
     {
         $configurator->addConfig($debugFile);
     }
 }
 $configurator->addConfig(__DIR__ . '/config/core.neon');
+
+if (PHP_SAPI === 'cli')
+{
+    $configurator->addConfig(__DIR__ . '/../vendor/nepttune/nepttune/config/cli.neon');
+    $configurator->addConfig(__DIR__ . '/config/cli.neon');
+}
 
 return $configurator->createContainer();
