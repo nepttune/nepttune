5b2ce0a25b8b220660f3780c77b5377b844d190c
Václav Pelíšek <peldax@gmail.com>
2018-10-10 12:25:03 +0200

    Create CronLogModel.php

---
+++ src/Model/CronLogModel.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of Nepttune (https://www.peldax.com)
+ *
+ * Copyright (c) 2018 Václav Pelíšek (info@peldax.com)
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.peldax.com>.
+ */
+ 
+declare(strict_types = 1);
+
+namespace Nepttune\Model;
+
+final class CronLogModel extends BaseModel
+{
+    const TABLE_NAME = 'log_cron';
+}
