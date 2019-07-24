9ef4ce5e390d952ea2f1104d5d9fbec839d0f3a7
Václav Pelíšek <peldax@gmail.com>
2019-07-25 01:46:45 +0200

    Update BaseListComponent.php

--- src/Component/BaseListComponent.php
+++ src/Component/BaseListComponent.php
@@ -56,104 +56,11 @@ abstract class BaseListComponent extends BaseComponent implements \Nepttune\TI\I
         $this->repository = $repository;
     }
 
-    protected function createComponentList() : DataGrid
-    {
-        $grid = new DataGrid();
-        $grid->setRememberState(false);
-        $grid->setDefaultPerPage(static::PER_PAGE_DEFAULT);
-        $grid->setTranslator($this->translator);
-
-        $grid->setDataSource($this->getDataSource());
-        $grid->setItemsPerPageList(static::PER_PAGE, $grid->getDataSource()->getCount() <= static::PER_PAGE_ALL_LIMIT);
-
-        $grid = $this->modifyList($grid);
-
-        if (static::ACTIVE && $this->activeSwitch)
-        {
-            $grid->addColumnStatus('active', 'list.column.active')
-                ->setSortable()
-                ->addOption(1, 'global.yes')
-                ->endOption()
-                ->addOption(0, 'global.no')
-                ->setClass('btn-danger')
-                ->endOption()
-                ->onChange[] = [$this, 'statusChange'];
-            
-            if (static::ACTIVE_FILTER)
-            {
-                $grid->addFilterSelect('active', 'list.column.active', [
-                    '' => $this->translator->translate('global.all'),
-                    1 => $this->translator->translate('global.yes'),
-                    0 => $this->translator->translate('global.no')
-                ]);
-            }
-        }
-
-        if ($this->inlineAdd)
-        {
-            $grid->addInlineAdd()
-                ->setTitle('global.add')
-                ->setClass('btn btn-md btn-primary ajax')
-                ->onControlAdd[] = [$this, 'modifyInlineForm'];
-            $grid->getInlineAdd()->onSubmit[] = [$this, 'saveInlineAdd'];
-        }
-
-        if ($this->inlineEdit)
-        {
-            $grid->addInlineEdit()
-                ->setIcon('pencil-alt')
-                ->setTitle('global.edit')
-                ->setClass('btn btn-xs btn-primary ajax')
-                ->onControlAdd[] = [$this, 'modifyInlineForm'];
-            $grid->getInlineEdit()->onSubmit[] = [$this, 'saveInlineEdit'];
-            $grid->getInlineEdit()->onSetDefaults[] = [$this, 'setInlineDefaults'];
-        }
-        
-        if ($this->add)
-        {
-            $grid->addToolbarButton($this->add === true ? ':add' : $this->add, 'global.add')
-                ->setIcon('plus')
-                ->setClass('btn btn-md btn-primary');    
-        }
-       
-        if ($this->edit)
-        {
-            $grid->addAction('edit', '', $this->edit === true ? ':edit' : $this->edit, ['id' => 'id'])
-                ->setIcon('pencil-alt')
-                ->setTitle('global.edit')
-                ->setClass('btn btn-xs btn-primary');
-        }
-
-        if ($this->delete)
-        {
-            $grid->addAction('delete', '', $this->delete === true ? 'delete!' : $this->delete)
-                ->setIcon('trash-alt')
-                ->setTitle('global.delete')
-                ->setClass('btn btn-xs btn-danger' . ($this->delete === true ? ' ajax' : ''))
-                ->setConfirmation(new \Ublaboo\DataGrid\Column\Action\Confirmation\StringConfirmation('global.confirm.delete'));
-        }
-
-        if (static::SORT)
-        {
-            $grid->setDefaultSort(static::SORT);
-        }
-        
-        if (static::FILTER)
-        {
-            $grid->setDefaultFilter(static::FILTER);
-        }
-
-        return $grid;
-    }
-
-    abstract protected function modifyList(DataGrid $grid) : DataGrid;
-
     public function getDataSource()
     {
         $all = $this->repository->findAll();
 
-        if (static::ACTIVE)
-        {
+        if (static::ACTIVE) {
             $activeCond = $this->activeSwitch ? 0 : 1;
             $all->where($this->repository->getName()  . '.active >= ?', $activeCond);
         }
@@ -163,8 +70,7 @@ abstract class BaseListComponent extends BaseComponent implements \Nepttune\TI\I
 
     public function statusChange(int $id, int $status, string $column = 'active') : void
     {
-        if (!\in_array($status, [0, 1], true))
-        {
+        if (!\in_array($status, [0, 1], true)) {
             throw new \Nette\Application\BadRequestException();
         }
 
@@ -174,12 +80,10 @@ abstract class BaseListComponent extends BaseComponent implements \Nepttune\TI\I
 
     public function handleDelete(int $id) : void
     {
-        if (static::ACTIVE)
-        {
+        if (static::ACTIVE) {
             $this->repository->setActive($id, -1);
         }
-        else
-        {
+        else {
             $this->repository->delete($id);
         }
         
@@ -208,5 +112,94 @@ abstract class BaseListComponent extends BaseComponent implements \Nepttune\TI\I
     public function modifyInlineForm(\Nette\Forms\Container $container) : void
     {
 
+    }
+
+    protected function createComponentList() : DataGrid
+    {
+        $grid = $this->createList();
+        $grid = $this->modifyList($grid);
+
+        if (static::ACTIVE && $this->activeSwitch) {
+            $grid->addColumnStatus('active', 'list.column.active')
+                ->setSortable()
+                ->addOption(1, 'global.yes')
+                ->endOption()
+                ->addOption(0, 'global.no')
+                ->setClass('btn-danger')
+                ->endOption()
+                ->onChange[] = [$this, 'statusChange'];
+
+            if (static::ACTIVE_FILTER) {
+                $grid->addFilterSelect('active', 'list.column.active', [
+                    '' => $this->translator->translate('global.all'),
+                    1 => $this->translator->translate('global.yes'),
+                    0 => $this->translator->translate('global.no')
+                ]);
+            }
+        }
+
+        if ($this->inlineAdd) {
+            $grid->addInlineAdd()
+                ->setTitle('global.add')
+                ->setClass('btn btn-md btn-primary ajax')
+                ->onControlAdd[] = [$this, 'modifyInlineForm'];
+            $grid->getInlineAdd()->onSubmit[] = [$this, 'saveInlineAdd'];
+        }
+
+        if ($this->inlineEdit) {
+            $grid->addInlineEdit()
+                ->setIcon('pencil-alt')
+                ->setTitle('global.edit')
+                ->setClass('btn btn-xs btn-primary ajax')
+                ->onControlAdd[] = [$this, 'modifyInlineForm'];
+            $grid->getInlineEdit()->onSubmit[] = [$this, 'saveInlineEdit'];
+            $grid->getInlineEdit()->onSetDefaults[] = [$this, 'setInlineDefaults'];
+        }
+
+        if ($this->add) {
+            $grid->addToolbarButton($this->add === true ? ':add' : $this->add, 'global.add')
+                ->setIcon('plus')
+                ->setClass('btn btn-md btn-primary');
+        }
+
+        if ($this->edit) {
+            $grid->addAction('edit', '', $this->edit === true ? ':edit' : $this->edit, ['id' => 'id'])
+                ->setIcon('pencil-alt')
+                ->setTitle('global.edit')
+                ->setClass('btn btn-xs btn-primary');
+        }
+
+        if ($this->delete) {
+            $grid->addAction('delete', '', $this->delete === true ? 'delete!' : $this->delete)
+                ->setIcon('trash-alt')
+                ->setTitle('global.delete')
+                ->setClass('btn btn-xs btn-danger' . ($this->delete === true ? ' ajax' : ''))
+                ->setConfirmation(new \Ublaboo\DataGrid\Column\Action\Confirmation\StringConfirmation('global.confirm.delete'));
+        }
+
+        if (static::SORT) {
+            $grid->setDefaultSort(static::SORT);
+        }
+
+        if (static::FILTER) {
+            $grid->setDefaultFilter(static::FILTER);
+        }
+
+        return $grid;
+    }
+
+    abstract protected function modifyList(DataGrid $grid) : DataGrid;
+
+    protected function createList() : DataGrid
+    {
+        $grid = new DataGrid();
+        $grid->setRememberState(false);
+        $grid->setDefaultPerPage(static::PER_PAGE_DEFAULT);
+        $grid->setTranslator($this->translator);
+
+        $grid->setDataSource($this->getDataSource());
+        $grid->setItemsPerPageList(static::PER_PAGE, $grid->getDataSource()->getCount() <= static::PER_PAGE_ALL_LIMIT);
+
+        return $grid;
     }
 }
